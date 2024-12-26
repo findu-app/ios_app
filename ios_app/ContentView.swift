04fc894a7e7f31129f17b6fb1fa8781b0e9ed2d2
Kenny Morales <knnymrls@outlook.com>
2024-12-26 10:52:01 -0600

    Initial Commit

---
+++ ios_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_app
+//
+//  Created by Kenny Morales on 12/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
