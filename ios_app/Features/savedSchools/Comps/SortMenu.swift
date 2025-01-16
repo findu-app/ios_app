f6e7be31ed662759f07fb97b2fec4c9316ebb9d9
Wilson Overfield <wkoverfield@gmail.com>
2025-01-16 11:51:50 -0600

    Improved sort menu to have sub menus.

--- ios_app/Features/savedSchools/Comps/SortMenu.swift
+++ ios_app/Features/savedSchools/Comps/SortMenu.swift
@@ -13,30 +13,54 @@ struct SortMenu: View {
 
     var body: some View {
         Menu {
-            Button("Name (A-Z)") { onSortSelected(.name, true) }
-            Button("Name (Z-A)") { onSortSelected(.name, false) }
-            Button("Average Debt (Low to High)") { onSortSelected(.averageDebt, true) }
-            Button("Average Debt (High to Low)") { onSortSelected(.averageDebt, false) }
-            Button("Admissions Rate (Low to High)") { onSortSelected(.admissionsRate, true) }
-            Button("Admissions Rate (High to Low)") { onSortSelected(.admissionsRate, false) }
-            Button("ACT Score (Low to High)") { onSortSelected(.actScore, true) }
-            Button("ACT Score (High to Low)") { onSortSelected(.actScore, false) }
-            Button("SAT Score (Low to High)") { onSortSelected(.satScore, true) }
-            Button("SAT Score (High to Low)") { onSortSelected(.satScore, false) }
-            Button("Average Aid (Low to High)") { onSortSelected(.averageAid, true) }
-            Button("Average Aid (High to Low)") { onSortSelected(.averageAid, false) }
-            Button("Average Net Price (Low to High)") { onSortSelected(.averageNetPrice, true) }
-            Button("Average Net Price (High to Low)") { onSortSelected(.averageNetPrice, false) }
-            Button("Cost of Attendance (Low to High)") { onSortSelected(.costOfAttendance, true) }
-            Button("Cost of Attendance (High to Low)") { onSortSelected(.costOfAttendance, false) }
-            Button("In-State Tuition (Low to High)") { onSortSelected(.inStateTuition, true) }
-            Button("In-State Tuition (High to Low)") { onSortSelected(.inStateTuition, false) }
-            Button("Out-of-State Tuition (Low to High)") { onSortSelected(.outStateTuition, true) }
-            Button("Out-of-State Tuition (High to Low)") { onSortSelected(.outStateTuition, false) }
-            Button("Size (Small to Large)") { onSortSelected(.size, true) }
-            Button("Size (Large to Small)") { onSortSelected(.size, false) }
-            Button("Student-to-Faculty Ratio (Low to High)") { onSortSelected(.studentToFacultyRatio, true) }
-            Button("Student-to-Faculty Ratio (High to Low)") { onSortSelected(.studentToFacultyRatio, false) }
+            Menu("Name") {
+                Button("Ascending") { onSortSelected(.name, true) }
+                Button("Descending") { onSortSelected(.name, false) }
+            }
+            Menu("Average Debt") {
+                Button("Low to High") { onSortSelected(.averageDebt, true) }
+                Button("High to Low") { onSortSelected(.averageDebt, false) }
+            }
+            Menu("Admissions Rate") {
+                Button("Low to High") { onSortSelected(.admissionsRate, true) }
+                Button("High to Low") { onSortSelected(.admissionsRate, false) }
+            }
+            Menu("ACT Score") {
+                Button("Low to High") { onSortSelected(.actScore, true) }
+                Button("High to Low") { onSortSelected(.actScore, false) }
+            }
+            Menu("SAT Score") {
+                Button("Low to High") { onSortSelected(.satScore, true) }
+                Button("High to Low") { onSortSelected(.satScore, false) }
+            }
+            Menu("Average Aid") {
+                Button("Low to High") { onSortSelected(.averageAid, true) }
+                Button("High to Low") { onSortSelected(.averageAid, false) }
+            }
+            Menu("Average Net Price") {
+                Button("Low to High") { onSortSelected(.averageNetPrice, true) }
+                Button("High to Low") { onSortSelected(.averageNetPrice, false) }
+            }
+            Menu("Cost of Attendance") {
+                Button("Low to High") { onSortSelected(.costOfAttendance, true) }
+                Button("High to Low") { onSortSelected(.costOfAttendance, false) }
+            }
+            Menu("In-State Tuition") {
+                Button("Low to High") { onSortSelected(.inStateTuition, true) }
+                Button("High to Low") { onSortSelected(.inStateTuition, false) }
+            }
+            Menu("Out-of-State Tuition") {
+                Button("Low to High") { onSortSelected(.outStateTuition, true) }
+                Button("High to Low") { onSortSelected(.outStateTuition, false) }
+            }
+            Menu("Size") {
+                Button("Small to Large") { onSortSelected(.size, true) }
+                Button("Large to Small") { onSortSelected(.size, false) }
+            }
+            Menu("Student-to-Faculty Ratio") {
+                Button("Low to High") { onSortSelected(.studentToFacultyRatio, true) }
+                Button("High to Low") { onSortSelected(.studentToFacultyRatio, false) }
+            }
         } label: {
             HStack(alignment: .center, spacing: 2) {
                 Text("Sort List")
